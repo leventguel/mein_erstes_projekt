90abf598136fe5614f66a729af3e920ddaa46a3d
Levent Guel <levent.guel@googlemail.com>
2024-11-25 11:29:16 +0100

    Lokale und globale Variablen geübt

--- main.py
+++ main.py
@@ -6,6 +6,25 @@
 True
 False
 
-[1,2,3,a,b]
+[1,2,3,"a","b"]
 
+""" Kommentare und Bezeicher """
 
+meine_globale_variable = 0 # eine globale variable
+
+print("Die Globale: {}".format(meine_globale_variable))
+
+def meine_beispiel_funktion ():
+	meine_globale_variable = 1
+	print("Die lokale ausgedruckte: ", meine_globale_variable)
+	return meine_globale_variable
+
+print("Die zurückgegebene: {}".format(meine_beispiel_funktion()))
+
+def eingabe_beispiel ():
+	meine_globale_variable = input("Bitte gib eine Zahl ein!: ")
+	print("lokal ausgedruckt: ", meine_globale_variable)
+	return "Rückgabewert: " + meine_globale_variable
+
+print("Die Globale ist immernoch: ", meine_globale_variable) # hier wird 0 zurückgegeben
+print(eingabe_beispiel())
